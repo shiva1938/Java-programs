bf3d8b32a582bd220a7a5eea3bc8c2fb504735a3
shiva1938 <shivashivam17@gmail.com>
2022-10-09 23:27:16 +0530

    Print array elemnts

---
+++ Array_programs/PrintArrayElements.java
@@ -0,0 +1,10 @@
+package Array_programs;
+
+public class PrintArrayElements {
+    public static void main(String[] args) {
+        int[] arr = {12,43,24,32,53,14};
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
