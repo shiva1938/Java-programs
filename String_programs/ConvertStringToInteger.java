1704d116b74e82ce51abe80a18fcdc6f0ae5a0d1
shiva1938 <shivashivam17@gmail.com>
2022-10-16 22:56:08 +0530

    convert string to number

---
+++ String_programs/ConvertStringToInteger.java
@@ -0,0 +1,9 @@
+package String_programs;
+
+public class ConvertStringToInteger {
+    public static void main(String[] args) {
+        String str = "2763";
+        Integer number = Integer.parseInt(str);
+        System.out.println("The converted string to number is: " + number);
+    }
+}
